05efe265f5e27be396e9b61b9faff001b58a9aa9
HaoYan Qi <ceo@spinachelectr.com>
2015-12-09 21:48:16 +0800

    Command Line tools

--- Assets/Scripts/Block System/Block Editor/ISSCEditorUserInterface.cs
+++ Assets/Scripts/Block System/Block Editor/ISSCEditorUserInterface.cs
@@ -15,8 +15,28 @@ public class ISSCEditorUserInterface : MonoBehaviour {
 		core.state = ISSCBEditorState.Deleting;
 	}
 
-	public void FillingBlockSelectorValueChange(string newValue){
-		core.currentFillingBlock = int.Parse (blockSelector.text);
+	public void Return(string v){
+		string command = blockSelector.text;
+
+		if (command.StartsWith ("select block")) {
+			string argu = command.Substring (12);
+			core.currentFillingBlock = int.Parse (argu);
+		} else if (command.StartsWith ("sb")) {
+			string argu = command.Substring (2);
+			core.currentFillingBlock = int.Parse (argu);
+		} else if (command.StartsWith ("save")) {
+			string argu = command.Substring (command.IndexOf("\"") + 1,command.LastIndexOf("\"") - command.IndexOf("\"") - 1);
+			core.SaveCurrentScene (Application.dataPath + "/Resources/SavedDatas/" + argu);
+		} else if (command.StartsWith ("load")) {
+			string argu = command.Substring (command.IndexOf("\"") + 1,command.LastIndexOf("\"") - command.IndexOf("\"") - 1);
+			core.OpenScene (Application.dataPath + "/Resources/SavedDatas/" + argu);
+		} else if (command.StartsWith ("new")) {
+			string argu = command.Substring (command.IndexOf("\"") + 1,command.LastIndexOf("\"") - command.IndexOf("\"") - 1);
+			core.NewScene (new ISSCBlockVector (21, 21, 21), argu);
+		}
+
+		Debug.Log ("Command Accepted... " + command);
+		blockSelector.text = "";
 	}
 
 	public void NewScene(){
