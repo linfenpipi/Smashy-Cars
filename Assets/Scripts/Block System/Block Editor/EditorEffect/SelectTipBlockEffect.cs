917058895c030934fb344c060348a076fee61cb5
0xLag <xiangsixing@qq.com>
2015-12-11 20:11:54 +0800

    Stable UI and some selecting behaviour

--- Assets/Scripts/Block System/Block Editor/EditorEffect/SelectTipBlockEffect.cs
+++ Assets/Scripts/Block System/Block Editor/EditorEffect/SelectTipBlockEffect.cs
@@ -1,21 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SelectTipBlockEffect : MonoBehaviour {
+public enum SelectBehaviour
+{
+	Change,
+	Move,
+	Sphere,
+	Cube
+}
+
+public class SelectTipBlockEffect : MonoBehaviour
+{
+	LayerMask maskedLayer;
+	LayerMask tipsBlockLayerMask;
+	public SelectBehaviour sb;
 
 	public AnimationCurve ac;
 	float DefaultSize;
 	float currentSize;
 
+	public Camera viewingCamera;
+
+
+
+
+	void Awake ()
+	{
+
+		tipsBlockLayerMask = 1 << gameObject.layer;
+
+		maskedLayer = 1 << ISSCLayerManager.blockLayer;
+	}
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		DefaultSize = 1.2f * ISSCBGrid.ISSC_BLOCK_UNIT_SIZE;
+		sb = SelectBehaviour.Change;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		currentSize = DefaultSize*ac.Evaluate(Time.time);
-		transform.localScale = Vector3.one*currentSize;
+	void Update ()
+	{
+		currentSize = DefaultSize * ac.Evaluate (Time.time);
+		transform.localScale = Vector3.one * currentSize;
 
+	}
+
+	void OnMouseDrag ()
+	{
+		Ray r1 = viewingCamera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit rh1;
+		Physics.Raycast (r1, out rh1, tipsBlockLayerMask);
+		Ray r2 = viewingCamera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit rh2;
+		Physics.Raycast (r2, out rh2, maskedLayer);
+		switch (sb) {
+		case SelectBehaviour.Change:
+			break;
+		case SelectBehaviour.Move:
+			break;
+		case SelectBehaviour.Sphere:
+			break;
+		case SelectBehaviour.Cube:
+			break;
+		default :
+			Debug.Log ("Null Select-Behaviour exist!");
+			break;
+		}
 	}
 }
